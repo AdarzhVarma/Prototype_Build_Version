bd12cb2bb86c2dc6e978093fbbda443d10985dc5
Adarzh_Varma <saiadarshvarma@gmail.com>
2025-11-02 23:27:15 +0100

    Conditional Check for testing

--- Emd _Boards/Emd_board1/EmdBoard.c
+++ Emd _Boards/Emd_board1/EmdBoard.c
@@ -4,5 +4,8 @@
 int main()
 {
     printf("This is the SW version for EMD Board that is fetched: %s\n",SW_BUILD_VERSION);
+    printf("This is the SW version for EMD Board second that is fetched: %s\n",SW_BUILD_VERSION);
+    
+
 
 }
