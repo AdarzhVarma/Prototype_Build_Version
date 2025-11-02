892eb38167f928a1a9d33dc5fb3dbbf1a1c8f0cc
Adarzh_Varma <saiadarshvarma@gmail.com>
2025-11-02 15:03:02 +0100

    Initial Commit with proto code of .c and .h files and the script for version update

---
+++ EmdBoard.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<EmdBoard.h>
+
+int main()
+{
+    printf("This is the SW version that is fetched: %s\n",SW_BUILD_VERSION);
+
+}
---
+++ EmdBoard.h
@@ -0,0 +1,4 @@
+#include<stdio.h>
+
+/* The build version shoudl look like Eg sdgfsd.01.02.2025*/
+#define SW_BUILD_VERSION "CommitID.Date" 
