501bf63aaa99ce7fa0ac81ebc804f84ee977c2bf
Adarzh_Varma <saiadarshvarma@gmail.com>
2025-11-13 00:03:45 +0100

    Minor test changes

--- Emd _Boards/Fuse_Board/FuseBoard.c
+++ Emd _Boards/Fuse_Board/FuseBoard.c
@@ -4,5 +4,5 @@
 int main()
 {
     printf("This is the SW version for FuseBoard that is fetched: %s\n",SW_BUILD_VERSION);
-
+    printf("This is the SW version V2  for FuseBoard that is fetched: %s\n",SW_BUILD_VERSION);
 }
